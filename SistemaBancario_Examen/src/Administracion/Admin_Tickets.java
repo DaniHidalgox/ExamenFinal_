fda2334d3467e1313677c5810b956e7e77dc9acc
Daniel Hidalgo <dm080039@gmail.com>
2023-08-21 13:52:02 -0600

    Se crea una interfaz Controller para generar distintas funciones

--- SistemaBancario_Examen/src/Administracion/Admin_Tickets.java
+++ SistemaBancario_Examen/src/Administracion/Admin_Tickets.java
@@ -9,5 +9,28 @@ package Administracion;
  * @author Daniel Hidalgo
  */
 public class Admin_Tickets {
+    private int numero;
+    private String Tipo;
+
+    public Admin_Tickets(int numero, String Tipo) {
+        this.numero = numero;
+        this.Tipo = Tipo;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getTipo() {
+        return Tipo;
+    }
+
+    public void setTipo(String Tipo) {
+        this.Tipo = Tipo;
+    }
     
 }
